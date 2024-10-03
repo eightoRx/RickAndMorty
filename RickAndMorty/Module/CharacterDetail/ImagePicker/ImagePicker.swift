0c0713154908933fe279fb5f80c92c5cb5a2f700
eightoRx <PavelKostin1988@yandex.ru>
2024-10-03 17:14:02 +0300

    draft camera and photo + alerts

--- RickAndMorty/Module/CharacterDetail/ImagePicker/ImagePicker.swift
+++ RickAndMorty/Module/CharacterDetail/ImagePicker/ImagePicker.swift
@@ -15,15 +15,23 @@ protocol ImagePickerDelegate: AnyObject {
 
 final class ImagePicker: NSObject {
     
+    enum SwitchAlert {
+        case camera
+        case photo
+    }
+    
+    var countCancellTapCamera = 0
+    var countCancellTapPhoto = 0
+    
     private weak var pickerController: UIImagePickerController?
     private weak var pHPController: PHPickerViewController?
     weak var delegate: ImagePickerDelegate?
     
-    func dismissPicker() {
+   func dismissPicker() {
         pickerController?.dismiss(animated: true)
     }
     
-    func dismissPHPicker() {
+   func dismissPHPicker() {
         pHPController?.dismiss(animated: true)
     }
     
@@ -58,46 +66,125 @@ final class ImagePicker: NSObject {
         }
     }
     
+    
     func showImagePicker(from viewController: UIViewController, allowsEditing: Bool) {
+     
         
-        let optionMenu = UIAlertController(title: nil, message: "Загрузите изображение", preferredStyle: .actionSheet)
+        // MARK: - Camera
+        let optionMenu = UIAlertController(title: nil, message: Constants.titleAlertLoad, preferredStyle: .actionSheet)
         
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let takePhotoAction = UIAlertAction(title: "Камера", style: .default) { [unowned self] _ in
-                self.presentPickerVC(from: viewController, sourceType: .camera, allowsEditing: allowsEditing)
+            let takePhotoAction = UIAlertAction(title: Constants.titleAlertCamera, style: .default) { [unowned self] _ in
+                AVCaptureDevice.requestAccess(for: .video) { camera in
+                    DispatchQueue.main.async {
+                        if camera {
+                            self.presentPickerVC(from: viewController, sourceType: .camera, allowsEditing: allowsEditing)
+                            
+                        } else {
+                            optionMenu.dismiss(animated: true) {
+                                self.customAlert(on: viewController, count: self.countCancellTapCamera, type: .camera)
+                            }
+                        }
+                    }
+                }
             }
             optionMenu.addAction(takePhotoAction)
         }
         
-        let takeGalleryImage = UIAlertAction(title: "Галерея", style: .default) { [unowned self] _ in
-            self.presentPHPicker(from: viewController)
+        // MARK: - Gallery
+        let takeGalleryImage = UIAlertAction(title: Constants.titleAlertGallery, style: .default) { [unowned self] _ in
+            checkPhotoLibraryPermission { success in
+                DispatchQueue.main.async {
+                    if success {
+                        self.presentPHPicker(from: viewController)
+                    } else {
+                        optionMenu.dismiss(animated: true) {
+                            self.customAlert(on: viewController, count: self.countCancellTapPhoto, type: .photo)
+                        }
+                    }
+                }
+            }
         }
+        
         optionMenu.addAction(takeGalleryImage)
         
-        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        let cancelAction = UIAlertAction(title: Constants.titleAlertCancel, style: .cancel)
         optionMenu.addAction(cancelAction)
         
         viewController.present(optionMenu, animated: true)
     }
+    
+    //MARK: - Check Photo status
+    func checkPhotoLibraryPermission(completion: @escaping (Bool) -> Void) {
+        let status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        
+        switch status {
+        case .authorized, .limited:
+            completion(true)
+        case . denied, .restricted:
+            completion(false)
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { newStatus in
+                DispatchQueue.main.async {
+                    completion(newStatus == .authorized || newStatus == .limited)
+                }
+            }
+        @unknown default:
+            completion(false)
+        }
+    }
+    
+    // MARK: - Custom alert for camera and photo library
+    func customAlert(on viewController: UIViewController, count: Int, type: SwitchAlert) {
+        var countCancell = count
+        
+        let alertAccess = UIAlertController(title: type == .camera ? Constants.titleAccessCamera : Constants.titleAccessPhoto,
+                                            message: type == .camera ? Constants.titleDescriptionAccessCamera : Constants.titleDescriptionAccessPhoto,
+                                            preferredStyle: .alert)
+        
+        let settingAction = UIAlertAction(title: Constants.titleAlertAccess,
+                                          style: .default,
+                                          handler: { _ in
+            if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSettings, options: [:])
+            }
+        })
+        
+        let cancelAction = UIAlertAction(title: Constants.titleAlertPhotoAndCameraCancel, style: .cancel) { _ in
+            countCancell += 1
+            if countCancell > 1 {
+                if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(appSettings, options: [:])
+                }
+            }
+        }
+        
+        alertAccess.addAction(settingAction)
+        alertAccess.addAction(cancelAction)
+        
+        viewController.present(alertAccess, animated: true)
+    }
 }
 
+// MARK: - extension ImagePicker
 extension ImagePicker: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let editImage = info[.editedImage] as? UIImage {
-            delegate?.imagePicker(self, didSelect: editImage)
-        } else if let originalImage = info[.originalImage] as? UIImage {
+        if let originalImage = info[.originalImage] as? UIImage {
             delegate?.imagePicker(self, didSelect: originalImage)
-        } else {
-            print("Image source not recognized")
         }
+        
+        picker.dismiss(animated: true)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         delegate?.cancellButtonDidClick(on: self)
+        
+        picker.dismiss(animated: true)
     }
 }
 
+// MARK: - extension PHPicker
 extension ImagePicker: PHPickerViewControllerDelegate {
     
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
@@ -114,61 +201,3 @@ extension ImagePicker: PHPickerViewControllerDelegate {
         }
     }
 }
-
-extension PHPhotoLibrary {
-    
-    static func execute(controller: UIViewController,
-                        onAccessHasBeenGranted: @escaping () -> Void,
-                        onAccessHasBeenDenied: (() -> Void)? = nil) {
-        let onDeniedOrRestricted = onAccessHasBeenDenied ?? {
-            let alert = UIAlertController(title: "Нужен доступ",
-                                          message: "Необходим доступ",
-                                          preferredStyle: .actionSheet)
-            
-            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-            alert.addAction(UIAlertAction(title: "Settings", style: .default) { _ in
-                if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
-                    UIApplication.shared.open(settingsURL)
-                }
-            })
-            DispatchQueue.main.async {
-                controller.present(alert, animated: true)
-            }
-        }
-        
-        
-        let status = PHPhotoLibrary.authorizationStatus(for: .addOnly)
-        switch status {
-        case .notDetermined:
-            onNotDetermined(onDeniedOrRestricted, onAccessHasBeenGranted)
-        case .denied, .restricted:
-            onDeniedOrRestricted()
-        case .authorized:
-            onAccessHasBeenGranted()
-        case .limited:
-            break
-        @unknown default:
-            break
-        }
-    }
-    
-    private static func onNotDetermined(_ onDeniedOrRestricted: @escaping (() -> Void),
-                                        _ onAuthorized: @escaping (() -> Void)) {
-        
-        PHPhotoLibrary.requestAuthorization(for: .addOnly) { status in
-            switch status {
-            case .notDetermined:
-                self.onNotDetermined(onDeniedOrRestricted, onAuthorized)
-            case .denied, .restricted:
-                onDeniedOrRestricted()
-            case .authorized:
-                onAuthorized()
-            case .limited:
-                break
-            @unknown default:
-                fatalError("PHPhotoLibrary::execute - case")
-            }
-        }
-    }
-}
-
